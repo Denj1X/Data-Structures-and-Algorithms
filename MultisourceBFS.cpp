aafcd20e616bcfad16dd1d579a3e14e1f16cc195
Matei Biciușcă <91908820+Denj1X@users.noreply.github.com>
2022-10-07 11:27:53 +0300

    Update MultisourceBFS.cpp

--- MultisourceBFS.cpp
+++ MultisourceBFS.cpp
@@ -5,15 +5,16 @@ class Graph {
 private:
     vector<int> *gr;
     int n;
-    vector<int> viz;
-    vector<int> dist;
-
+    vector<int> viz; ///vector de vizitati pentru graf
+    vector<int> dist; ///vector de distante pentru graf
+    
 public:
     Graph(vector<int> *gr_, int n_): gr(gr_), n(n_) {
         viz.resize(n + 1, 0);
         dist.resize(n + 2, 0);
     }
 
+    ///parcurgere basic BFS, cu retinerea distantei
     void Multisource_BFS(queue<int>q) {
         while(!q.empty()) {
             int k = q.front();
@@ -28,13 +29,15 @@ public:
         }
     }
 
+    ///functia pentru rezolvarea problemei
     vector<int> nearestTown(int n,int *sources,int s) {
         queue<int> q;
-
         for(int i = 0;i < s; i++) {
+            ///punem in queue punctele de start date
             q.push(sources[i]);
             viz[sources[i]]=true;
         }
+        ///incepem BFS-ul din punctele respective
         Multisource_BFS(q);
         return dist;
     }
